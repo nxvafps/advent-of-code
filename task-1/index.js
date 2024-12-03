5f7258e0047cceb3b12c14fb7ce55ccba71ee293
nxvafps <oliverquirk11@gmail.com>
2024-12-03 20:00:56 +0000

    Complete task 1 part 2

--- task-1/index.js
+++ task-1/index.js
@@ -1,3 +1,4 @@
+// PART 1
 const input = require("./input.js");
 
 const lines = input.trim().split("\n");
@@ -18,11 +19,20 @@ let list = leftList.map((value, index) => [value, rightList[index]]);
 let diffs = list.map((pair) => Math.abs(pair[0] - pair[1]));
 let totaldiff = diffs.reduce((sum, diff) => sum + diff, 0);
 
-console.log("Sorted lists:");
-list.forEach((pair) => console.log(pair[0], pair[1]));
+//console.log("Sorted lists:");
+//list.forEach((pair) => console.log(pair[0], pair[1]));
 
-console.log("Differences:");
-console.log(diffs);
+//console.log(`Differences: ${diffs}`);
 
-console.log("Total difference:");
-console.log(totaldiff);
+console.log(`Total difference: ${totaldiff}`);
+
+// PART 2
+let similarityScores = [];
+leftList.forEach((value) => {
+  let count = rightList.filter((rightValue) => rightValue === value).length;
+  similarityScores.push(value * count);
+});
+
+let similarityScore = similarityScores.reduce((sum, score) => sum + score, 0);
+
+console.log(`similarity score: ${similarityScore}`);
